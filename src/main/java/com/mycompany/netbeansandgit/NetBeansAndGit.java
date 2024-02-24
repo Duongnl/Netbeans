025d732bbc626569198a3d03771ce33e235ee86b
Duongg <107763946+Duongnl@users.noreply.github.com>
2024-02-24 10:44:58 +0700

    Update NetBeansAndGit.java
    Delete hello

--- src/main/java/com/mycompany/netbeansandgit/NetBeansAndGit.java
+++ src/main/java/com/mycompany/netbeansandgit/NetBeansAndGit.java
@@ -11,8 +11,6 @@ package com.mycompany.netbeansandgit;
 public class NetBeansAndGit {
 
     public static void main(String[] args) {
-        System.out.println("Xin chao cac ban");
-        System.out.println("Xin chao cac ban");
-        System.out.println("Xin chao cac ban");
+        
     }
 }
