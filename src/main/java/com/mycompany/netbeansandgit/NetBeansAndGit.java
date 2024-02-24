a7bcf23c5644eab0baa48b476fc64c3bc3854ce1
Duongg <107763946+Duongnl@users.noreply.github.com>
2024-02-24 11:38:26 +0700

    hello singapore

--- src/main/java/com/mycompany/netbeansandgit/NetBeansAndGit.java
+++ src/main/java/com/mycompany/netbeansandgit/NetBeansAndGit.java
@@ -11,6 +11,6 @@ package com.mycompany.netbeansandgit;
 public class NetBeansAndGit {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world singapore");
     }
 }
